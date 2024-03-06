b2c505d8afa7c7d288951c31bc58df758db30a22
Yeongu Choe <yeongu.choe@icloud.com>
2024-03-06 17:19:58 +0900

    롬복 추가

---
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// @Setter
+public class HelloLombok {
+    private String text;
+    private int id;
+
+    public static void main(String[] args) {
+
+    }
+
+}
